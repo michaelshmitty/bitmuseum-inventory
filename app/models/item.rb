4f312667677598e242ae00c6661bb299d47bbf71
Michael Smith <m@michaelsmith.be>
2020-01-03 22:39:31 +0100

    Add extra fields to Item.
    Fixes #1
    Fixes #2
    Fixes #5
    Fixes #6
    Fixes #11

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,17 @@
 class Item < ApplicationRecord
+  AUTHENTICITY_STATES = %w[original replica]
+
+  include AASM
+
   belongs_to :user
   belongs_to :category
   has_many_attached :images
 
   validates :name, presence: true
+
+  # NOTE(m): State machine
+  aasm do
+    state :unknown, initial: true
+    state :working, :needs_cleaning, :needs_repair, :broken
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_27_210620) do
+ActiveRecord::Schema.define(version: 2020_01_03_203418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,12 @@ ActiveRecord::Schema.define(version: 2019_11_27_210620) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "category_id", null: false
+    t.text "model_number"
+    t.text "serial_number"
+    t.string "authenticity"
+    t.text "location"
+    t.string "aasm_state"
+    t.index ["aasm_state"], name: "index_items_on_aasm_state"
     t.index ["category_id"], name: "index_items_on_category_id"
     t.index ["name"], name: "index_items_on_name"
     t.index ["user_id"], name: "index_items_on_user_id"
