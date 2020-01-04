a716a1111f031c72f1e3999f8b3222bceedc9506
Michael Smith <m@michaelsmith.be>
2020-01-04 13:40:56 +0100

    Cleanup: Delete obsolete migrations.

--- db/migrate/20200104111022_add_owner_to_item.rb
+++
@@ -1,7 +0,0 @@
-class AddOwnerToItem < ActiveRecord::Migration[6.0]
-  def change
-    # TODO(m): Fix this null: false when every Item has an owner.
-    # add_reference :items, :owner, null: false, foreign_key: true
-    add_reference :items, :owner, foreign_key: true
-  end
-end
--- db/migrate/20200104123306_change_owner_id_on_item.rb
+++
@@ -1,5 +0,0 @@
-class ChangeOwnerIdOnItem < ActiveRecord::Migration[6.0]
-  def change
-    change_column :items, :owner_id, :integer, null: false
-  end
-end
