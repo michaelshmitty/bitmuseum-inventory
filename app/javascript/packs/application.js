a79f62bfef850164f939cf202eb3a711d5f03122
Koen De Voegt <koen@devoegt.be>
2020-01-15 21:56:30 +0100

    aligned buttons

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,7 +17,7 @@ require("channels")
 // const imagePath = (name) => images(name, true)
 
 import 'bootstrap'
-import 'bootswatch/dist/sketchy/bootstrap.min.css';
+import 'bootswatch/dist/cosmo/bootstrap.min.css';
 
 document.addEventListener("turbolinks:load", () => {
   $('[data-toggle="tooltip"]').tooltip()
